6b2c841352a18194ca9a2d62ddfd84ff525bb01b
Niels Koster <n.koster@portavita.eu>
2020-02-27 23:25:25 +0100

    Use interface

--- filterByTerm.ts
+++ filterByTerm.ts
@@ -1,10 +1,22 @@
-function filterByTerm(input, searchTerm) {
-    if (!searchTerm) throw Error("searchTerm cannot be empty")
-    if (!input.length) throw Error("inputArr cannot be empty")
-    const regex = new RegExp(searchTerm, "i")
+interface Link {
+  url: string,
+  id?: number,
+  description?: string
+}
+
+const filterByTerm = (input: Array<Link>, searchTerm: string): Array<Link> => {
+    if (!searchTerm) throw Error('searchTerm cannot be empty')
+    if (!input.length) throw Error('inputArr cannot be empty')
+    const regex = new RegExp(searchTerm, 'i')
     return input.filter(function(arrayElement) {
         return arrayElement.url.match(regex)
     });
   }
-  
-filterByTerm("input string", "java")
+
+const obj1: Link = { url: "string1" }
+const obj2: Link = { url: "string2" }
+const obj3: Link = { url: "string3" }
+
+const arrOfLinks: Array<Link> = [obj1, obj2, obj3]
+
+console.log(filterByTerm(arrOfLinks, 'g2'))
