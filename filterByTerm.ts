b973ef03b2d73ea2a7db22eaf1a82bfac2ce4d9a
Niels Koster <niels.koster@portavita.nl>
2020-02-28 11:48:34 +0100

    Indent

--- filterByTerm.ts
+++ filterByTerm.ts
@@ -9,13 +9,13 @@ interface TranslatedLink extends Link {
 }
 
 const filterByTerm = (input: Array<Link>, searchTerm: string): Array<Link> => {
-    if (!searchTerm) throw Error('searchTerm cannot be empty')
-    if (!input.length) throw Error('inputArr cannot be empty')
-    const regex = new RegExp(searchTerm, 'i')
-    return input.filter(function(arrayElement) {
-        return arrayElement.url.match(regex)
-    })
-  }
+  if (!searchTerm) throw Error('searchTerm cannot be empty')
+  if (!input.length) throw Error('inputArr cannot be empty')
+  const regex = new RegExp(searchTerm, 'i')
+  return input.filter(function(arrayElement) {
+    return arrayElement.url.match(regex)
+  })
+}
 
 const obj1: Link = { url: "string1" }
 const obj2: Link = { url: "string2" }
